cc63ee4a797b7676f8b8c19e32102239d625f61a
dltmddyd321 <dltmddyd321@naver.com>
2021-08-19 17:59:10 +0900

    2021_08_09
    Slot Machine
    
    1. Layout Design
    
    2. Obj / Interface
    
    3. Inflate

---
+++ SlotMachine/app/src/main/java/com/example/slotmachine/ImageViewScroll/Util.kt
@@ -0,0 +1,10 @@
+package com.example.slotmachine.ImageViewScroll
+
+object Util {
+    var bar = 0
+    var lemon = 1
+    var orange = 2
+    var seven = 3
+    var triple = 4
+    var watermelon = 5
+}
