4e00700510bdb125b05e8604a14717513195edaa
dltmddyd321 <dltmddyd321@naver.com>
2021-09-20 00:38:55 +0900

    2021_09_20
    View Binding 처리 중 컴퓨터 오류 발생

--- MartCommunity/app/src/main/java/com/example/martcommunity/home/HomeFragment.kt
+++ MartCommunity/app/src/main/java/com/example/martcommunity/home/HomeFragment.kt
@@ -1,7 +1,20 @@
 package com.example.martcommunity.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.martcommunity.R
+import com.example.martcommunity.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+    }
+
 }
