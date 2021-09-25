ff4fa15a45a6b506a18e0199ff8b8b1392071349
dltmddyd321 <dltmddyd321@naver.com>
2021-09-25 16:10:19 +0900

    2021_09_25
    1. Adapter 구성 완료
    
    2. View Model 간단 구성 완료
    
    3. Firebase Project 연결 완료

---
+++ MartCommunity/app/src/main/java/com/example/martcommunity/home/ArticleModel.kt
@@ -0,0 +1,9 @@
+package com.example.martcommunity.home
+
+data class ArticleModel (
+    val sellerId: String,
+    val title: String,
+    val createdAt:Long,
+    val price: String,
+    val imageUrl: String
+)
--- MartCommunity/app/src/main/java/com/example/martcommunity/home/HomeFragment.kt
+++ MartCommunity/app/src/main/java/com/example/martcommunity/home/HomeFragment.kt
@@ -2,19 +2,29 @@ package com.example.martcommunity.home
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.martcommunity.R
 import com.example.martcommunity.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
 
     private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val fragmentHomeBinding = FragmentHomeBinding.bind(view)
         binding = fragmentHomeBinding
+
+        articleAdapter = ArticleAdapter()
+
+        //RecyclerView를 지정하여 프래그먼트와 바인딩 연결
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        //값을 배정하기 위한 Adapter 연결
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
     }
 
 }
