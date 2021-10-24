edb951173e804b11117a0f90045d119639d4e945
dltmddyd321 <dltmddyd321@naver.com>
2021-10-24 19:41:42 +0900

    2021_10_24
    사진 보정 애플리케이션
    
    1. 하단 리사이클러뷰를 이용하여 좌우 스와이프 메뉴 디자인 설정
    
    2. 갤러리 저장소 접근 권한 허가를 통해 사진 수정 버튼 클릭 시 실제 디바이스 내 갤러리 열고 특정 사진을 가져오도록 구현

--- PicApp/app/src/main/java/com/example/picapp/EditImageActivity.kt
+++ PicApp/app/src/main/java/com/example/picapp/EditImageActivity.kt
@@ -1,11 +1,42 @@
 package com.example.picapp
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.picapp.databinding.ActivityEditImageBinding
+import com.example.picapp.databinding.ActivityMainBinding
 
 class EditImageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditImageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_image)
+        binding = ActivityEditImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+        displayImagePreview()
+    }
+
+    private fun displayImagePreview() {
+        //객체 자체를 Intent 전달
+        intent.getParcelableExtra<Uri>(MainActivity.KEY_IMAGE_URI)?.let { imageUri ->
+            //App 외부 저장소 데이터를 가져오기 위한 contentResolver
+            val inputStream = contentResolver.openInputStream(imageUri)
+            
+            //가져온 이미지 데이터를 저장할 변수
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+
+            binding.imgPreview.setImageBitmap(bitmap)
+            binding.imgPreview.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setListeners() {
+        binding.imgBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- PicApp/app/src/main/java/com/example/picapp/MainActivity.kt
+++ PicApp/app/src/main/java/com/example/picapp/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.picapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import com.example.picapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //갤러리 접근 권한을 위한 상수 선언
+    companion object {
+        private const val REQUEST_CODE_PICK_IMAGE = 1
+        const val KEY_IMAGE_URI = "imageUri"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+    }
+
+    //접근 권한 허가 처리 함수
+    private fun setListeners() {
+        binding.btnEditNewImg.setOnClickListener {
+            Intent(
+                //갤러리 접근을 위한 액션 Intent
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            ).also { pickerIntent ->
+                pickerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivityForResult(pickerIntent, REQUEST_CODE_PICK_IMAGE)
+            }
+        }
+    }
+
+    //접근 권한 허가를 받고 갤러리 연결 후 다시 App Activity 복귀 시 발동
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK) {
+            data?.data?.let { imageUri ->
+                Intent(applicationContext, EditImageActivity::class.java).also { editImageIntent ->
+                    editImageIntent.putExtra(KEY_IMAGE_URI, imageUri)
+                    startActivity(editImageIntent)
+                }
+            }
+        }
     }
 }
