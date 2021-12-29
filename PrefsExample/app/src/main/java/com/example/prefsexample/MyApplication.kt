258087925399f9c8abdf7d21162f73acff253b8b
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2021-12-29 16:59:06 +0900

    SharedPreference

---
+++ PrefsExample/app/src/main/java/com/example/prefsexample/MyApplication.kt
@@ -0,0 +1,15 @@
+package com.example.prefsexample
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+    companion object {
+        lateinit var prefs : PreferenceUtil
+    }
+
+    override fun onCreate() {
+        prefs = PreferenceUtil(applicationContext)
+        super.onCreate()
+    }
+}
