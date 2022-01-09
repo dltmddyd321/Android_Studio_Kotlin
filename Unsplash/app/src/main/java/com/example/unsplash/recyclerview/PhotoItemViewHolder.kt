f1bf067c58f6ef5f6061bc1898adb53ac6007073
dltmddyd321 <dltmddyd321@naver.com>
2022-01-09 21:54:02 +0900

    Recyclerview / Menu-Bar

---
+++ Unsplash/app/src/main/java/com/example/unsplash/recyclerview/PhotoItemViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.unsplash.recyclerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}
