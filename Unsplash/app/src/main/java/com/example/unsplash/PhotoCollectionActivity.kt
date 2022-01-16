367397f1bc65af7d0ccbc356b85b5e481331f442
dltmddyd321 <dltmddyd321@naver.com>
2022-01-16 21:37:09 +0900

    Custom-ViewHolder / Glide

--- Unsplash/app/src/main/java/com/example/unsplash/PhotoCollectionActivity.kt
+++ Unsplash/app/src/main/java/com/example/unsplash/PhotoCollectionActivity.kt
@@ -3,11 +3,15 @@ package com.example.unsplash
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.unsplash.model.Photo
+import com.example.unsplash.recyclerview.PhotoGridRecyclerviewAdapter
+import kotlinx.android.synthetic.main.activity_photo_collection.*
 
 class PhotoCollectionActivity: AppCompatActivity() {
 
-    var photoList = ArrayList<Photo>()
+    private var photoList = ArrayList<Photo>()
+    private lateinit var photoGridRecyclerviewAdapter: PhotoGridRecyclerviewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +21,10 @@ class PhotoCollectionActivity: AppCompatActivity() {
         val searchTerm = intent.getStringExtra("searchTerm")
 
         photoList = bundle?.getSerializable("photoArrayList") as ArrayList<Photo>
+
+        this.photoGridRecyclerviewAdapter = PhotoGridRecyclerviewAdapter()
+        this.photoGridRecyclerviewAdapter.submitList(photoList)
+        photoRecyclerview.layoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+        photoRecyclerview.adapter = this.photoGridRecyclerviewAdapter
     }
 }
--- Unsplash/app/src/main/java/com/example/unsplash/recyclerview/PhotoItemViewHolder.kt
+++ Unsplash/app/src/main/java/com/example/unsplash/recyclerview/PhotoItemViewHolder.kt
@@ -1,7 +1,33 @@
 package com.example.unsplash.recyclerview
 
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideExtension
+import com.bumptech.glide.annotation.GlideOption
+import com.example.unsplash.App
+import com.example.unsplash.R
+import com.example.unsplash.model.Photo
+import com.example.unsplash.utils.Constants
+import kotlinx.android.synthetic.main.layout_photo_item.view.*
 
 class PhotoItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val photoImageView = itemView.photoImg
+    private val photoCreatedText = itemView.createdText
+    private val photoLikesCntText = itemView.likeCntText
+
+    fun bindWithView(photoItem: Photo) {
+        Log.d(Constants.TAG, "PhotoItemViewHolder - bindWithView() called")
+
+        photoCreatedText.text = photoItem.createdAt
+        photoLikesCntText.text = photoItem.likesCount.toString()
+
+        Glide.with(App.instance)
+            .load(photoItem.thumbnail)
+            .placeholder(R.drawable.ic_baseline_insert_photo_24)
+            .into(photoImageView)
+    }
+
 }
