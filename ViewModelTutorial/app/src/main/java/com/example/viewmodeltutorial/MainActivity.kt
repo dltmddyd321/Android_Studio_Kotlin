6187888efddece840efefc401bc9ca2a48f7e261
dltmddyd321 <dltmddyd321@naver.com>
2022-03-12 16:23:08 +0900

    LiveData-ViewModel

--- ViewModelTutorial/app/src/main/java/com/example/viewmodeltutorial/MainActivity.kt
+++ ViewModelTutorial/app/src/main/java/com/example/viewmodeltutorial/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.viewmodeltutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var numberViewModel: NumberViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberViewModel = ViewModelProvider(this)[NumberViewModel::class.java]
+
+        numberViewModel.currentValue.observe(this, Observer {
+            number_textView.text = it.toString()
+        })
+
+        plus.setOnClickListener(this)
+        minus.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        val userInput = user_input.text.toString().toInt()
+
+        when(view) {
+            plus ->
+                numberViewModel.updateValue(ActionType.PLUS, userInput)
+            minus ->
+                numberViewModel.updateValue(ActionType.MINUS, userInput)
+        }
     }
 }
