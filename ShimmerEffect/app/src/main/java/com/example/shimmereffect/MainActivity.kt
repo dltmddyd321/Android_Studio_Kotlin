cafa4aaeb40af0ee3d235a669de8e2729e835400
이승용 <dltmddyd321@naver.com>
2022-03-25 16:29:28 +0900

    Shimmer-Effect
    1. Shimmer-Effect (RecyclerView)
    2. Life-Cycle-Scope

---
+++ ShimmerEffect/app/src/main/java/com/example/shimmereffect/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.shimmereffect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import com.example.shimmereffect.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.rvSample.adapter = SampleAdapter()
+
+        loadSampleData()
+    }
+
+    private fun loadSampleData() {
+        lifecycleScope.launch {
+            showSampleData(true)
+            delay(3000)
+
+            val samples = getSampleList()
+            (binding.rvSample.adapter as SampleAdapter).replaceAll(samples)
+
+            showSampleData(false)
+
+        }
+    }
+
+    private fun showSampleData(isLoading : Boolean) {
+        if(isLoading) {
+            binding.sflSample.startShimmer()
+            binding.sflSample.visibility = View.VISIBLE
+            binding.rvSample.visibility = View.GONE
+        } else {
+            binding.sflSample.stopShimmer()
+            binding.sflSample.visibility = View.GONE
+            binding.rvSample.visibility = View.VISIBLE
+        }
+    }
+}
