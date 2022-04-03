cf972ef219ee438ab1a401abc6073e195c5062fe
dltmddyd321 <dltmddyd321@naver.com>
2022-04-03 19:05:33 +0900

    ViewModel

--- Contacts2/app/src/main/java/com/example/contacts/MainActivity.kt
+++ Contacts2/app/src/main/java/com/example/contacts/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.contacts
 
+import android.arch.lifecycle.ViewModelProviders
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var contactViewModel : ContactViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contactViewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
+
+        contactViewModel.getAll().observe(this, Observer<List<Contact>> { contact ->
+            //Update UI
+        })
     }
 }
