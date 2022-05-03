6a077016827b669590b1c2587203e453ad55f810
dltmddyd321 <dltmddyd321@naver.com>
2022-05-03 21:37:18 +0900

    Html-Parsing

--- JsonWebView/app/src/main/java/com/example/jsonwebview/MainActivity.kt
+++ JsonWebView/app/src/main/java/com/example/jsonwebview/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.jsonwebview
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
-import android.webkit.CookieManager
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
+import org.jsoup.Jsoup
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -35,20 +37,36 @@ class MainActivity : AppCompatActivity() {
         webSettings.userAgentString = USER_AGENT
 
         //웹뷰의 쿠키를 강제로 삭제하기
-        CookieManager.getInstance().removeAllCookies(null)
-        CookieManager.getInstance().flush()
-
-        webView.loadUrl(URL)
+//        CookieManager.getInstance().removeAllCookies(null)
+//        CookieManager.getInstance().flush()
         webView.webViewClient = JsonWebViewClient()
+        webView.addJavascriptInterface(MyJavaScriptInterface(), "Android")
+        webView.loadUrl(URL)
         setContentView(webView)
 
-        executorService.execute {
-//            val doc = Jsoup.connect(URL).get()
-//            Log.d("TestPlay", doc.toString())
-            val result = getJsonObject(URL)
-            Log.d("TestPlay", result.toString())
-            runOnUiThread {
-                Toast.makeText(this, result.getString("limit_date"), Toast.LENGTH_SHORT).show()
+//        executorService.execute {
+////            val doc = Jsoup.connect(URL).get()
+////            Log.d("TestPlay", doc.toString())
+//            val result = getJsonObject(URL)
+//            Log.d("TestPlay", result.toString())
+//            runOnUiThread {
+//                Toast.makeText(this, result.getString("limit_date"), Toast.LENGTH_SHORT).show()
+//            }
+//        }
+    }
+
+    inner class MyJavaScriptInterface {
+        @RequiresApi(Build.VERSION_CODES.N)
+        @JavascriptInterface
+        fun getHtml(html : String) {
+            Log.d("자바스크립트", html)
+            val result = Html.fromHtml(html, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+            Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT).show()
+            Log.d("result", result)
+            html.let {
+                val body = Jsoup.parse(it).body()
+                val pre = body.select("pre").first()
+                Log.d("pre", pre.toString())
             }
         }
     }
@@ -66,6 +84,7 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPageFinished(view: WebView?, url: String?) {
             Log.d("WebName", "Result : $url")
+            view?.loadUrl("javascript:window.Android.getHtml(document.getElementsByTagName('html')[0].innerHTML);")
         }
     }
 
