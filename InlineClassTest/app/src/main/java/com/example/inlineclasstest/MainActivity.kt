135ff5a5becd9ac86b19e47e2317d7ad3c2f4ade
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2022-05-15 18:08:32 -0700

    Coroutine-Exception-Handling

--- InlineClassTest/app/src/main/java/com/example/inlineclasstest/MainActivity.kt
+++ InlineClassTest/app/src/main/java/com/example/inlineclasstest/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.inlineclasstest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
+import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d("TEST", "IO Thread")
+            var cnt = 0
+
+            kotlin.runCatching {
+                cnt ++
+            }.onSuccess {
+                Log.d("TEST", "$cnt")
+            }.onFailure {
+                Log.d("TEST", "$cnt")
+            }.fold({
+                   // 성공시에만 실행
+            }, {
+                    //실패
+            })
+        }
     }
 }
