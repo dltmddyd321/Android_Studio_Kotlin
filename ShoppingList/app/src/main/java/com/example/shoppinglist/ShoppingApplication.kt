56748bc460a929ca9fedeeb3f97c398146236096
dltmddyd321 <dltmddyd321@naver.com>
2022-06-06 17:34:04 +0900

    DI - Kodein

---
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingApplication.kt
@@ -0,0 +1,22 @@
+package com.example.shoppinglist
+
+import android.app.Application
+import com.example.shoppinglist.data.db.ShoppingDatabase
+import com.example.shoppinglist.data.repository.ShoppingRepository
+import com.example.shoppinglist.ui.ShoppingViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class ShoppingApplication : Application(), KodeinAware {
+    override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@ShoppingApplication))
+        bind() from singleton { ShoppingDatabase(instance()) }
+        bind() from singleton { ShoppingRepository(instance()) }
+        bind() from provider { ShoppingViewModelFactory(instance()) }
+    }
+}
--- ShoppingList/app/src/main/java/com/example/shoppinglist/ui/ShoppingActivity.kt
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ui/ShoppingActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.ShoppingItem
@@ -13,16 +12,19 @@ import com.example.shoppinglist.data.db.ShoppingDatabase
 import com.example.shoppinglist.data.repository.ShoppingRepository
 import com.example.shoppinglist.others.ShoppingItemAdapter
 import kotlinx.android.synthetic.main.activity_shopping.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
+
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
-
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProvider(this, factory)[ShoppingViewModel::class.java]
 
