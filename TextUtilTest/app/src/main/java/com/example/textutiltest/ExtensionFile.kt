0ed4b1904ad6944e914c7f455c872fad9131c760
이승용 <dltmddyd321@naver.com>
2022-06-07 11:22:55 +0900

    Extenstion-Function

---
+++ TextUtilTest/app/src/main/java/com/example/textutiltest/ExtensionFile.kt
@@ -0,0 +1,26 @@
+package com.example.textutiltest
+
+import java.util.*
+
+class Person(val name: String) {
+    fun walk() = "person is walking"
+}
+
+fun Person.walk() = "unknown function"
+fun Person.nameToUpperCase() = this.name.uppercase(Locale.getDefault())
+
+fun main() {
+    val person = Person("leesy")
+    println(person.nameToUpperCase())
+
+    //The member method takes precedence.
+    println(person.walk())
+}
+
+/*
+#Extension Function
+1. Extended Target Class.Function Name()
+2. If an extension function is defined in 'Any' type, it can be used in all objects.
+3. In the case of the extension function, private and protected members that can only be used internally are not accessible.
+4. The member method takes precedence.
+ */
