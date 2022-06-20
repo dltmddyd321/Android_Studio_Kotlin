d4a382dcdd38c3478c3b1a7cc2b1f852c67d02cf
dltmddyd321 <dltmddyd321@naver.com>
2022-06-20 20:58:42 +0900

    StateFlow-Basic
    - SnackBar
    - ViewModelScope
    - StateFlow
    - Sealed Class

--- StateFlow/app/src/main/java/com/example/stateflow/MainViewModel.kt
+++ StateFlow/app/src/main/java/com/example/stateflow/MainViewModel.kt
@@ -1,11 +1,26 @@
 package com.example.stateflow
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    private val _loginUiState = MutableStateFlow<>()
+    private val _loginUiState = MutableStateFlow<LoginUiState>(LoginUiState.Empty)
+    val loginUiState: StateFlow<LoginUiState> = _loginUiState
+
+    fun login(userName: String, password: String) = viewModelScope.launch {
+        _loginUiState.value = LoginUiState.Loading
+        delay(2000L)
+        if (userName == "android" && password == "topSecret") {
+            _loginUiState.value = LoginUiState.Success
+        } else {
+            _loginUiState.value = LoginUiState.Error("Wrong credentials")
+        }
+    }
 
     sealed class LoginUiState {
         object Success : LoginUiState()
