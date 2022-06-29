e098bdde9faa10d9e395051dd84c61ba1b1aa239
dltmddyd321 <dltmddyd321@naver.com>
2022-06-29 21:37:49 +0900

    Base-abstractClass
    + <data></data> Tag

--- CheckEvent/presentation/src/main/java/com/example/presentation/view/MainActivity.kt
+++ CheckEvent/presentation/src/main/java/com/example/presentation/view/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.presentation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.presentation.R
+import com.example.presentation.databinding.ActivityMainBinding
+import com.example.presentation.view.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun init() {
+
     }
 }
