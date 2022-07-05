c423b02de0ce3aa0a01bb6dda5ccefd1c27b7e6d
dltmddyd321 <dltmddyd321@naver.com>
2022-07-05 21:32:27 +0900

    Navigation-Connect

--- CheckEvent/presentation/src/main/java/com/example/presentation/view/ManNameFragment.kt
+++ CheckEvent/presentation/src/main/java/com/example/presentation/view/ManNameFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentManNameBinding
 import com.example.presentation.view.base.BaseFragment
 
 class ManNameFragment : BaseFragment<FragmentManNameBinding>(R.layout.fragment_man_name) {
     override fun init() {
+        binding.fragment = this
     }
 
+    fun nextBtn(view: View) {
+        this.findNavController().navigate(R.id.action_manNameFragment_to_resultFragment)
+    }
 }
--- CheckEvent/presentation/src/main/java/com/example/presentation/view/ResultFragment.kt
+++ CheckEvent/presentation/src/main/java/com/example/presentation/view/ResultFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentResultBinding
 import com.example.presentation.view.base.BaseFragment
 
 class ResultFragment : BaseFragment<FragmentResultBinding>(R.layout.fragment_result) {
     override fun init() {
+        binding.fragment = this
     }
 
+    fun backMainBtn(view: View) {
+        this.findNavController().navigate(R.id.action_resultFragment_to_mainFragment)
+    }
 }
