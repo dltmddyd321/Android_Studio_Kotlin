a17842a07b34750b6076cc72f6abfac791396bdf
이승용 <dltmddyd321@naver.com>
2022-08-03 10:29:11 +0900

    Text-Watcher

--- TextUtilTest/app/src/main/java/com/example/textutiltest/MainActivity.kt
+++ TextUtilTest/app/src/main/java/com/example/textutiltest/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.textutiltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.SpannableStringBuilder
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.example.textutiltest.databinding.ActivityMainBinding
 import org.w3c.dom.Text
@@ -24,5 +27,19 @@ class MainActivity : AppCompatActivity() {
         val token = cutText.split(" ")
 
         Log.d("CUT", token.toString())
+
+        binding.editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("beforeTextChanged", " s: $s, start: $start, count: $count, after: $after")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("onTextChanged", "s: $s, start: $start, before: $before, count: $count")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("afterTextChanged", "s: $s")
+            }
+        })
     }
 }
