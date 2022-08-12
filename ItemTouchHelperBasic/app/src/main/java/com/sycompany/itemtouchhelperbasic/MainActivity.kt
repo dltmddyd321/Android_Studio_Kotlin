2b3ea61987eadababf9ae077c245a3e18639c490
이승용 <dltmddyd321@naver.com>
2022-08-12 11:37:37 +0900

    OnScroll-Listener

--- ItemTouchHelperBasic/app/src/main/java/com/sycompany/itemtouchhelperbasic/MainActivity.kt
+++ ItemTouchHelperBasic/app/src/main/java/com/sycompany/itemtouchhelperbasic/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sycompany.itemtouchhelperbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +21,18 @@ class MainActivity : AppCompatActivity() {
         val touchHelper = ItemTouchHelper(callback)
         touchHelper.attachToRecyclerView(recyclerView)
         recyclerView.adapter = adapter
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                Log.d("SCROLL", "x : $dx / y : $dy")
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                Log.d("SCROLL", "state : $newState")
+            }
+        })
 
         adapter.startDrag(object : LinearListViewAdapter.OnStartDragListener {
             override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
