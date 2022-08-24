7371b4a78cbe53dc2204f9295dae5936608c08c2
이승용 <dltmddyd321@naver.com>
2022-08-24 16:34:22 +0900

    Compare-Sort

--- TextUtilTest/app/src/main/java/com/example/textutiltest/MainActivity.kt
+++ TextUtilTest/app/src/main/java/com/example/textutiltest/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.example.textutiltest.databinding.ActivityMainBinding
 import org.w3c.dom.Text
+import java.util.*
+import kotlin.Comparator
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,8 +47,32 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, nullTest(), Toast.LENGTH_SHORT).show()
     }
 
-    private fun nullTest() : String {
+    private fun nullTest(): String {
         var msg: String? = null
         return msg!!
     }
+
+    data class Person(var name: String, var age: Int)
+
+    private fun personSort() {
+        val arr =
+            arrayOf(Person("Park", 10), Person("Kim", 12), Person("Choi", 5), Person("Lee", 19))
+
+        //오름차순 정렬
+        arr.sortWith { o1, o2 ->
+            o1.age - o2.age
+        }
+
+        println(arr.contentToString())
+    }
+
+    private fun personCompare() {
+        val arr =
+            arrayOf(Person("Park", 10), Person("Kim", 12), Person("Kim", 5), Person("Lee", 19))
+
+        arr.sortWith(compareBy({ it.name }, { it.age })) //먼저 이름 기준 오름차순 정렬, 이후 나이 기준 오름차순 정렬
+
+        println(arr.contentToString())
+    }
 }
+
