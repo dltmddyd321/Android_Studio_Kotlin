0d088517a46675286c3bc6b165e799eb0abb2814
이승용 <dltmddyd321@naver.com>
2022-09-02 17:06:00 +0900

    Android-Timer-Test

--- ViewModelTimer/app/src/main/java/com/sycompany/viewmodeltimer/MainActivity.kt
+++ ViewModelTimer/app/src/main/java/com/sycompany/viewmodeltimer/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.sycompany.viewmodeltimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.sycompany.viewmodeltimer.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var timerViewModel: TimerViewModel
     lateinit var binding : ActivityMainBinding
+    private var cnt = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +22,21 @@ class MainActivity : AppCompatActivity() {
 
         binding.lifecycleOwner = this
         setContentView(binding.root)
+
+
+        val timer = Timer()
+        val timerTask: TimerTask = object : TimerTask() {
+            override fun run() {
+                cntWork(timer)
+            }
+        }
+        timer.schedule(timerTask, 0, 3000)
+
+    }
+
+    private fun cntWork(timer: Timer) {
+        Log.d("TIMER","${cnt}Work!")
+        if (cnt >= 10) timer.cancel()
+        cnt ++
     }
 }
