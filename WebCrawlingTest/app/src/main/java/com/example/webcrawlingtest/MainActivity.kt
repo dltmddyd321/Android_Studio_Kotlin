8bb244684af783c4e3e17c3562e0d1962c81d52c
dltmddyd321 <dltmddyd321@naver.com>
2022-10-21 21:15:19 +0900

    Web-Crawling

---
+++ WebCrawlingTest/app/src/main/java/com/example/webcrawlingtest/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.webcrawlingtest
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+import org.jsoup.Jsoup
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val searchSite = "https://google.co.kr/search?q="
+        val keyword = "naver"
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val doc = Jsoup.connect(searchSite + keyword).get()
+            withContext(CoroutineScope(Dispatchers.Main).coroutineContext) {
+                Log.d("TEST", doc.toString())
+            }
+        }
+    }
+}
