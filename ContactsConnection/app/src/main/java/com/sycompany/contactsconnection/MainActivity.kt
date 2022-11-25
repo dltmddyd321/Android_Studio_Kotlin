b2ffab83a4b23794fe0888db8a0e5978bbee5d35
이승용 <dltmddyd321@naver.com>
2022-11-25 16:42:32 +0900

    정규식 활용 (2)

--- ContactsConnection/app/src/main/java/com/sycompany/contactsconnection/MainActivity.kt
+++ ContactsConnection/app/src/main/java/com/sycompany/contactsconnection/MainActivity.kt
@@ -37,22 +37,41 @@ class MainActivity : AppCompatActivity() {
         val peopleName = cursor?.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME) ?: 0
         if (cursor != null) {
             while (cursor.moveToNext()) {
-                val bDay = cursor.getString(bDayColumn).checkValidNumbers()
+                val bDay = cursor.getString(bDayColumn)
                 val pName = cursor.getString(peopleName)
+                "--05-22".convertToDateArray()
                 Log.i("연락처 가져오기 테스트", "Contact Result: Birth $bDay Who $pName")
                 val birthCal = Calendar.getInstance()
-                val birthMonth = bDay.substring(bDay.length - 4 until bDay.length - 2)
-                val birthDate = bDay.substring(bDay.length - 2 until bDay.length)
-                birthCal.set(Calendar.MONTH, birthMonth.toInt() - 1)
-                birthCal.set(Calendar.DATE, birthDate.toInt())
-                Log.i("연락처 가져오기 테스트", "날짜 확인 : ${birthCal.time}")
+//                val birthMonth = bDay.substring(bDay.length - 4 until bDay.length - 2)
+//                val birthDate = bDay.substring(bDay.length - 2 until bDay.length)
+//                birthCal.set(Calendar.MONTH, birthMonth.toInt() - 1)
+//                birthCal.set(Calendar.DATE, birthDate.toInt())
+//                Log.i("연락처 가져오기 테스트", "날짜 확인 : ${birthCal.time}")
             }
         }
     }
 
     private fun String.checkValidNumbers(): String {
-        val regexPickNumber = "[^0-9]".toRegex()
+        val regexPickNumber = "^\\d{2,3} - \\d{3,4} - \\d{4}\$".toRegex()
         return this.replace(regexPickNumber, "")
+    }
+
+    private fun String.convertToDateArray(): List<String> {
+        val dateList = mutableListOf<String>()
+        val regexDateConverter: Regex
+        val matchResult: MatchResult
+
+        if (this.startsWith("-")) {
+            regexDateConverter = "^-[-/]?(\\d{2})[-/]?(\\d{2})$".toRegex()
+            matchResult = regexDateConverter.find(this) ?: return emptyList()
+            dateList.add("1920")
+            dateList.addAll(matchResult.destructured.toList())
+        } else {
+            regexDateConverter = "^(\\d{4})[-/]?(\\d{2})[-/]?(\\d{2})$".toRegex()
+            matchResult = regexDateConverter.find(this) ?: return emptyList()
+            dateList.addAll(matchResult.destructured.toList())
+        }
+        return dateList
     }
 
     private fun getBirthByContacts(): Cursor? {
