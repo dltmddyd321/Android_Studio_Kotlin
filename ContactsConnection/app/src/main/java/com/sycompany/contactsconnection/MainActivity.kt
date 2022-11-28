8ae81a96bf78e4e08a92215728c8ae7e8fb8b2c9
이승용 <dltmddyd321@naver.com>
2022-11-28 17:11:13 +0900

    특정 연락처에 대해서만 Intent
    ContactsContract.PhoneLookup.LOOKUP_KEY,
                ContactsContract.PhoneLookup._ID,
    ContactsContract.Contacts.getLookupUri

--- ContactsConnection/app/src/main/java/com/sycompany/contactsconnection/MainActivity.kt
+++ ContactsConnection/app/src/main/java/com/sycompany/contactsconnection/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sycompany.contactsconnection
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
@@ -12,18 +13,48 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.util.Calendar
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private var mBtnLogin: Button? = null
+    private var gBtn: Button? = null
 
+    private var lookupId: String = ""
+    private var contactIdKey: Long = 0L
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mBtnLogin = findViewById(R.id.contactBtn)
+        gBtn = findViewById(R.id.contactGoBtn)
         mBtnLogin?.setOnClickListener {
             getContacts()
+        }
+
+
+
+        val uri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Phone.CONTENT_FILTER_URI, Uri.encode("content://com.android.contacts/display_photo/4"))
+
+
+
+        val phoneUri = ContactsContract.Contacts.getLookupUri(54600, "0r6666-271C73DB188FB3.847r12337-271C73DB188FB3")
+
+        gBtn?.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_VIEW)
+//            intent.data = Uri.withAppendedPath(
+//                ContactsContract.Contacts.CONTENT_URI,
+//                java.lang.String.valueOf(contactIdKey)
+//            )
+//            this.startActivity(intent)
+
+            Log.i("연락처 경로 확인", "$phoneUri")
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = phoneUri
+            this.startActivity(intent)
         }
     }
 
@@ -35,12 +66,18 @@ class MainActivity : AppCompatActivity() {
         val cursor = getBirthByContacts()
         val bDayColumn = cursor?.getColumnIndex(ContactsContract.CommonDataKinds.Event.START_DATE) ?: 0
         val peopleName = cursor?.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME) ?: 0
+        val contactId = cursor?.getColumnIndex(ContactsContract.PhoneLookup._ID) ?: 0
+        val look = cursor?.getColumnIndex(ContactsContract.PhoneLookup.LOOKUP_KEY) ?: 0
         if (cursor != null) {
             while (cursor.moveToNext()) {
                 val bDay = cursor.getString(bDayColumn)
                 val pName = cursor.getString(peopleName)
+                val contacts = cursor.getLong(contactId)
+                val lookup = cursor.getString(look)
                 "--05-22".convertToDateArray()
-                Log.i("연락처 가져오기 테스트", "Contact Result: Birth $bDay Who $pName")
+                Log.i("연락처 가져오기 테스트", "Contact Result: Birth $bDay Who $pName contact $contacts lookup $lookup")
+                lookupId = lookup
+                contactIdKey = contacts
                 val birthCal = Calendar.getInstance()
 //                val birthMonth = bDay.substring(bDay.length - 4 until bDay.length - 2)
 //                val birthDate = bDay.substring(bDay.length - 2 until bDay.length)
@@ -78,6 +115,8 @@ class MainActivity : AppCompatActivity() {
         val uri: Uri = ContactsContract.Data.CONTENT_URI
 
         val projection = arrayOf(
+            ContactsContract.PhoneLookup.LOOKUP_KEY,
+            ContactsContract.PhoneLookup._ID,
             ContactsContract.Contacts.DISPLAY_NAME,
             ContactsContract.CommonDataKinds.Event.START_DATE
         )
