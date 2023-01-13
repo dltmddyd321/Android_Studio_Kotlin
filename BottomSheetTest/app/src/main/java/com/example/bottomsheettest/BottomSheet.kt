4ab1cf35511a8046acf0fb4cf6005ddab89f9729
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-01-12 19:45:46 -0800

    Custom-BottomSheet

---
+++ BottomSheetTest/app/src/main/java/com/example/bottomsheettest/BottomSheet.kt
@@ -0,0 +1,32 @@
+package com.example.bottomsheettest
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class BottomSheet : BottomSheetDialogFragment() {
+    var sheetBehavior: BottomSheetBehavior<*>? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    var mBottomSheetBehaviorCallback: BottomSheetCallback = object : BottomSheetCallback() {
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss()
+            }
+        }
+
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun setupDialog(dialog: Dialog, style: Int) {
+        super.setupDialog(dialog, style)
+    }
+}
