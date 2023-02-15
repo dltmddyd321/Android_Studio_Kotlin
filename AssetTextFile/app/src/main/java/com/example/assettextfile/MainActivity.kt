7589848e2d5133078afb17a9698eaf403e722c4e
이승용 <73208493+dltmddyd321@users.noreply.github.com>
2023-02-15 14:35:28 +0900

    사진 촬영 후 갤러리 저장 과정

--- AssetTextFile/app/src/main/java/com/example/assettextfile/MainActivity.kt
+++ AssetTextFile/app/src/main/java/com/example/assettextfile/MainActivity.kt
@@ -6,21 +6,27 @@ import android.content.res.AssetManager
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.*
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var currentPhotoPath: String
     lateinit var assetManager: AssetManager
     lateinit var inputStream: InputStream
     lateinit var bitmap: Bitmap
+    private val REQUEST_TAKE_PHOTO = 1
 
     private val activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == RESULT_OK && it.data != null) {
@@ -45,6 +51,39 @@ class MainActivity : AppCompatActivity() {
         btnPhoto.setOnClickListener {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             activityResult.launch(intent)
+        }
+    }
+
+    private fun createImgFile(): File {
+        // Create an image file name
+        val nowTime = System.currentTimeMillis()
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${nowTime}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun dispatchTakePicture() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePicIntent ->
+            takePicIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImgFile()
+                } catch (e: IOException) {
+                    null
+                }
+                photoFile?.also {
+                    val photoUri = FileProvider.getUriForFile(
+                        this, "com.example.android.fileprovider", it
+                    )
+                    takePicIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                    startActivityForResult(takePicIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
         }
     }
 
