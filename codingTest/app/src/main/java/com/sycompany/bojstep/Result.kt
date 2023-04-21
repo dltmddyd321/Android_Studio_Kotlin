0096e4ef7e7fba7b765dd157525cede1a596315f
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-04-20 23:34:21 -0700

    기본 정렬

--- codingTest/app/src/main/java/com/sycompany/bojstep/Result.kt
+++ codingTest/app/src/main/java/com/sycompany/bojstep/Result.kt
@@ -1812,3 +1812,14 @@ fun priorityQueue() {
     pqAscending.poll()
     pqDescending.remove()
 }
+
+fun basicSort() {
+    val aar = arrayListOf<Pair<Int, Int>>()
+    aar.sortWith { o1, o2 ->
+        if (o1.second == o2.second) { //second 끼리 같으면 first 를 비교하고
+            o1.first.compareTo(o2.first)
+        } else {
+            o1.second.compareTo(o2.second) //다르면 second 를 비교한다.
+        }
+    }
+}
