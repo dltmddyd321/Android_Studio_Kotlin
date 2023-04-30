3f7d4d3445f0b67b04ac0c542e6bcaca9c9afb7f
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-04-29 17:25:14 -0700

    BFS DFS 정리

--- codingTest/app/src/main/java/com/sycompany/bojstep/BaseExtensions.kt
+++ codingTest/app/src/main/java/com/sycompany/bojstep/BaseExtensions.kt
@@ -145,7 +145,8 @@ class BaseExtensions {
             customerList.associateBy { it.id } //안의 값을 기준으로 Iterable<T> -> K의 연산에 대해 Map<K, T>를 반환 /Map<R, Customer> 형태를 반환
         val associateByResult2 =
             customerList.associateBy(Customer::id, Customer::name) // /Map<id, name> 의 형태를 반환
-        val associateByResult3 = customerList.associate { it.id to it.name } // /Map<id, name> 의 형태를 반환
+        val associateByResult3 =
+            customerList.associate { it.id to it.name } // /Map<id, name> 의 형태를 반환
     }
 }
 
@@ -156,6 +157,7 @@ fun isPrime(n: Int): Boolean {
     }
     return true
 }
+
 //root 를 사용하지 않는 판별
 fun isPrimeWithoutSqrt(n: Int): Boolean {
     var i = 2
@@ -163,4 +165,91 @@ fun isPrimeWithoutSqrt(n: Int): Boolean {
         if (n % i++ == 0) return false
     }
     return true
+}
+
+fun binarySearch(arr: IntArray, target: Int): Int {
+    var low = 0
+    var high = arr.lastIndex
+    var mid = 0
+
+    while (low <= high) {
+        mid = (low + high) / 2
+
+        when {
+            arr[mid] == target -> return mid
+            arr[mid] > target -> high = mid - 1
+            else -> low = mid + 1
+        }
+    }
+    return -1
+}
+
+fun dfsWithCall(
+    graph: Array<MutableList<Int>>,
+    depthList: ArrayList<Int>,
+    check: BooleanArray,
+    depth: Int,
+    v: Int
+) {
+    for (node in graph[v]) {
+        if (!check[node - 1]) {
+            check[node - 1] = true
+            depthList.add(depth)
+            dfsWithCall(graph, depthList, check, depth, v)
+            check[node - 1] = false
+        }
+    }
+}
+
+fun bfsWithRepeat(
+    graph: Array<MutableList<Int>>,
+    n: Int,
+    v: Int
+): String { //node starting vertex, graph는 간선을 기준으로 연결된 애들을 갖고있는다.
+    graph.forEach {
+        it.sort()
+    }
+    var result = "$v"
+    val queue = mutableListOf<Int>()
+    val check = Array(n + 1) { false }
+    queue.add(v)
+    check[v] = true
+    while (queue.isNotEmpty()) {
+        val current = queue.removeFirst()
+        graph[current].forEach {
+            if (!check[it]) {
+                queue.add(it)
+                result += " $it"
+                check[it] = true
+            }
+        }
+    }
+    return result
+}
+
+//graph 에는 각 node 가 Index 형태로 표현됨 (0 노드는 0 번째 arr 에 연결된 node 정보를 mutablelist 로 가짐)
+//전체 개수 n
+//시작하는 vertex v
+fun dfsWithRepeat(graph: Array<MutableList<Int>>, n: Int, v: Int): String {
+    graph.forEach {
+        it.sortDescending()
+    }
+    var result = "$v"
+    val stack = mutableListOf<Int>() //sequence
+    val check = Array(n + 1) { false }
+    stack.add(v)
+    check[v] = true
+    while (stack.isNotEmpty()) {
+        val current = stack.removeLast()
+        if (!check[current] && current != v) {
+            result += " $current"
+            check[current] = true
+        }
+        graph[current].forEach {
+            if (!check[it]) {
+                stack.add(it)
+            }
+        }
+    }
+    return result
 }
