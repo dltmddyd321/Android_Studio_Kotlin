ff1e7edcdeb2c43f5242160b0084d05acd062989
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-05-02 22:36:02 -0700

    Compose-Button 기본

--- ComposeStudy/app/src/main/java/com/example/composestudy/MainActivity.kt
+++ ComposeStudy/app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.composestudy
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,11 +29,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeStudyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                ButtonExample {
+                    Toast.makeText(this, "Send Clicked!", Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -71,6 +69,35 @@ fun Greeting(name: String) {
             textDecoration = TextDecoration.Underline,
             textAlign = TextAlign.Center
         )
+    }
+}
+
+@Composable
+fun ButtonExample(onClicked: () -> Unit) {
+    Button(
+        onClick = { onClicked.invoke() },
+        enabled = false
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Send,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+        Text(text = "Send")
+    }
+    Button(
+        onClick = { onClicked.invoke() },
+        enabled = true,
+        border = BorderStroke(10.dp, Color.Magenta),
+        shape = CircleShape,
+        contentPadding = PaddingValues(20.dp) //내부 내용 가장자리
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Send,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+        Text(text = "Send")
     }
 }
 
