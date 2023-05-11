51e549378bd698e676e841b49326008647655eb7
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-05-10 22:46:13 -0700

    Compose System Bar Color

--- ComposeStudy/app/src/main/java/com/example/composestudy/ui/theme/Theme.kt
+++ ComposeStudy/app/src/main/java/com/example/composestudy/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -33,6 +35,17 @@ fun ComposeStudyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Comp
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    val systemUiController = rememberSystemUiController()
+    if (darkTheme) {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent
+        )
+    } else {
+        systemUiController.setSystemBarsColor(
+            color = Color.White
+        )
     }
 
     MaterialTheme(
