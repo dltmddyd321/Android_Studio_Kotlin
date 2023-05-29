55ba4ef5ace036ffaf7a319d20ca2232ad4a5cbb
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-05-28 17:42:03 -0700

    n번 뒤 문자열

--- ProgrammersTest/app/src/main/java/com/example/programmerstest/LevelOne.kt
+++ ProgrammersTest/app/src/main/java/com/example/programmerstest/LevelOne.kt
@@ -3,11 +3,23 @@ package com.example.programmerstest
 import kotlin.math.max
 
 fun main() {
-    println(morePlus(9, 91))
+    println(behindN("ProgrammerS123", 11))
 }
 
 fun morePlus(a: Int, b: Int): Int {
     val first = (a.toString() + b.toString()).toInt()
     val second = (b.toString() + a.toString()).toInt()
     return max(first, second)
+}
+
+fun behindN(my_string: String, n: Int): String {
+    val length = my_string.toCharArray().size
+    val check = length - n
+    var res = ""
+
+    my_string.toCharArray().forEachIndexed { index, c ->
+        if (index >= check) res += c
+    }
+
+    return res
 }
