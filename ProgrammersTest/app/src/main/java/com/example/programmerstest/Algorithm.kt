494565d0fe773f33a7b24032fc0fdcbde964a532
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-07-20 22:53:04 -0700

    스택, 큐, 우선순위 큐

--- ProgrammersTest/app/src/main/java/com/example/programmerstest/Algorithm.kt
+++ ProgrammersTest/app/src/main/java/com/example/programmerstest/Algorithm.kt
@@ -1,5 +1,8 @@
 package com.example.programmerstest
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 class Algorithm {
 
     var arrNum = arrayListOf(5, 2, 3, 4, 1)
@@ -52,5 +55,57 @@ class Algorithm {
             }
             arrNum[last] = temp
         }
+    }
+
+    /*
+    - 삽입: Insertion O(1)
+    - 삭제: Deletion O(1)(pop) / O(N)(remove)
+    - 검색: Search O(N)
+     */
+    fun stack() {
+        val s = Stack<Int>()
+
+        s.push(1) // 객체를 스택에 추가
+        s.push(3)
+
+        s.peek() // 맨 위의 객체 반환 (비어 있는 상태이면 EmptyStackException 발생)
+        println(s)
+
+        s.pop() // 맨 위의 객체 삭제하고 반환 (비어 있는 상태이면 EmptyStackException 발생)
+        println(s)
+    }
+
+    /*
+    - 삽입: Insertion O(1)(enqueue)
+    - 삭제: Deletion O(1)(dequeue) / O(N)(remove)
+    - 검색: Search O(N)
+     */
+    fun queue() {
+        val q : Queue<Int> = LinkedList() // 큐로 선언하고 LinkedList 로 할당
+        q.add(1)    // 객체를 큐에 추가 (큐가 가득찬 상태이면 illegalStateException 발생)
+        q.offer(3)  // 객체를 큐에 추가 (큐가 가득찬 상태이면 false 반환)
+        println(q)
+
+        println(q.element())    // 맨 앞 객체 리턴 (큐가 비어있는 상태이면 NoSuchElementException 발생)
+        println(q.elementAt(1)) // 인덱스 값의 객체 리턴
+        println(q.peek())   // 맨 앞 객체 리턴 (큐가 비어있는 상태이면 false 반환)
+
+        q.remove()  // 삭제하면서 객체 반환 (큐가 비어있는 상태이면 NoSuchElementException 발생)
+        val tmp = q.poll()  // 삭제하면서 객체 반환 (큐가 비어있는 상태이면 false 반환)
+        println(q)
+        println("두번째 삭제한 객체 : $tmp")
+    }
+
+    fun priorityQueue() { //우선 순위 큐
+        val q = PriorityQueue<Int>() // 오름차순
+        val q2 = PriorityQueue<Int>(Collections.reverseOrder()) // 내림차순
+
+        q.addAll(listOf(1,5,2,4,3))
+        q2.addAll(listOf(1,5,2,4,3))
+
+        // 우선 순위가 높은 것부터 반환
+        while(!q.isEmpty()) print("${q.poll()} ")
+        println()
+        while(!q2.isEmpty()) print("${q2.poll()} ")
     }
 }
