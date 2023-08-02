1c8f594d4f29ef46ccbb431e15d114484de49948
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-08-01 22:37:11 -0700

    viewState에 따른 화면 이동 처리

--- NasaDataService/app/src/main/java/com/example/nasadataservice/ui/view/ManifestScreen.kt
+++ NasaDataService/app/src/main/java/com/example/nasadataservice/ui/view/ManifestScreen.kt
@@ -3,7 +3,10 @@ package com.example.nasadataservice.ui.view
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.nasadataservice.domain.model.RoverManifestUiState
 import com.example.nasadataservice.ui.manifest.MarsRoverManifestViewModel
 
 @Composable
@@ -11,9 +14,17 @@ fun ManifestScreen(
     roverName: String?,
     marsRoverManifestViewModel: MarsRoverManifestViewModel
 ) {
+    val viewState by marsRoverManifestViewModel.roverManifestUiState.collectAsStateWithLifecycle()
+
     if (!roverName.isNullOrEmpty()) {
         LaunchedEffect(Unit) {
             marsRoverManifestViewModel.getMarsRoverManifest(roverName)
+        }
+
+        when (val roverManifestUiState = viewState) {
+            RoverManifestUiState.Error -> Error()
+            RoverManifestUiState.Loading -> Loading()
+            is RoverManifestUiState.Success -> ManifestList(roverManifestUiModelList = roverManifestUiState.roverManifestUiModel)
         }
     }
     Text(text = roverName ?: "")
