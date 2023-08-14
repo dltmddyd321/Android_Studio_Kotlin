d8d7c0c833e1aca5a168e359c0e2b85eb795f28f
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-08-13 22:51:15 -0700

    분수 더하기
    - 최소공배수 활용

--- ProgrammersTest/app/src/main/java/com/example/programmerstest/LevelOne.kt
+++ ProgrammersTest/app/src/main/java/com/example/programmerstest/LevelOne.kt
@@ -315,3 +315,16 @@ fun chunkedArray(num_list: IntArray, n: Int): Array<IntArray> {
     }
     return result.toTypedArray()
 }
+
+fun solution(denum1: Int, num1: Int, denum2: Int, num2: Int): IntArray {
+    val denum3 = (num1 * denum2) + (num2 * denum1)
+    val num3 = num1 * num2
+    var min = 1 // 최소 공배수
+
+    for (i in min..denum3) {
+        if (denum3 % i == 0 && num3 % i == 0) {
+            min = i
+        }
+    }
+    return intArrayOf(denum3 / min, num3 / min)
+}
