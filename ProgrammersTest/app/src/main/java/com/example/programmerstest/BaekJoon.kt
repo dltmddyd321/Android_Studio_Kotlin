8ee7ee0e135f167ddca126b5f492ddc01e86ce31
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-02-10 11:59:50 +0900

    10815 숫자카드 풀이 중

--- ProgrammersTest/app/src/main/java/com/example/programmerstest/BaekJoon.kt
+++ ProgrammersTest/app/src/main/java/com/example/programmerstest/BaekJoon.kt
@@ -79,7 +79,7 @@ fun main211() {
     println(input[0].toLong() + input[1].toLong() + input[2].toLong())
 }
 
-fun main() = with(Scanner(System.`in`)) {
+fun main22() = with(Scanner(System.`in`)) {
     val dial = arrayOf("ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ")
     val word = next().toString()
     var res = 0
@@ -109,7 +109,87 @@ fun main323() = with(Scanner(System.`in`)) {
     buckets.forEach { print("$it ") }
 }
 
-fun main22() = with(Scanner(System.`in`)) {
+fun main121() = with(Scanner(System.`in`)) {
+
+    fun sums(number: Int): Int {
+        var sum = 0
+        for (c in number.toString()) sum += c - '0'
+        return sum
+    }
+
+    val n = nextInt()
+    val length = n.toString().length
+    for (i in n - 9 * length..n) {
+        if (sums(i) + i == n) {
+            println(i)
+            return@with
+        }
+    }
+    println(0)
+}
+
+fun main564() = with(Scanner(System.`in`)) {
+    val goods = arrayListOf(1, 1, 2, 2, 2, 8)
+    repeat(6) {
+        goods[it] = goods[it] - nextInt()
+    }
+    println(goods.joinToString(" "))
+}
+
+fun main98() = with(Scanner(System.`in`)) {
+    val paper = Array(101) { Array(101) { false } }
+
+    repeat(nextInt()) {
+        val (p, q) = next().toString().split(" ").map { it.toInt() }
+
+        repeat(10) { x ->
+            repeat(10) { y ->
+                paper[x + p][y + q] = true //색종이의 범위에 들어가는 부분은 true (중복 범위는 신경 안써도 됨)
+            }
+        }
+    }
+
+    println(paper.sumOf { it.count { value -> value } }) //true인 부분 모두 카운트 계산
+}
+
+//fun main() = with(System.`in`.bufferedReader()) {
+//    val n = readLine().toInt()
+//
+//    val map = Array(101) { Array(101) { false } }
+//
+//    repeat(n) {
+//        val (p1, p2) = readLine().split(" ").map { it.toInt() }
+//
+//        repeat(10) { dx ->
+//            repeat(10) { dy ->
+//                map[p1 + dx][p2 + dy] = true
+//            }
+//        }
+//    }
+//
+//    println(map.sumOf { it.count { it } })
+//}
+
+
+fun main1311() = with(Scanner(System.`in`)) {
+    val a = nextInt()
+    val b = nextInt()
+    val c = nextInt()
+    val d = nextInt()
+    val e = nextInt()
+    val f = nextInt()
+
+    for (i in -999 until 1000) {
+        for (j in -999 until 1000) {
+            if (a * i + b * j == c && d * i + e * j == f) {
+                println("$i $j")
+                break
+            }
+        }
+    }
+}
+
+fun main2122() = with(Scanner(System.`in`)) {
     var res = ""
     val word = Array(5) { CharArray(15) { ' ' } }
     var maxSize = 0
@@ -146,6 +226,30 @@ fun main6() = with(Scanner(System.`in`)) {
     }
 
     println(resArr.joinToString(" "))
+}
+
+fun main() = with(Scanner(System.`in`)) {
+//    val having = mutableListOf<Int>()
+//    val size = nextInt()
+//    repeat(size) {
+//        having.add(nextInt())
+//    }
+//    val check = mutableListOf<Int>()
+//    val checkSize = nextInt()
+//    repeat(checkSize) {
+//        if (having.contains(nextInt())) {
+//            check.add(1)
+//        } else check.add(0)
+//    }
+//    println(check.joinToString(" "))
+
+    val n = nextInt()
+    val cardSet = next().split("").map { it.toInt() }.toMutableSet()
+    val m = nextInt()
+
+    next().split("").map { it.toInt() }.forEach {
+        if (!cardSet.add(it)) println("1") else println("0")
+    }
 }
 
 fun main7() = with(Scanner(System.`in`)) {
