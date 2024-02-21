83e1a7be9b2048edce822765ffdd944ab8b3b76a
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-02-21 21:16:43 +0900

    Compose View Pager 구현

---
+++ MviComposeBasic/app/src/main/java/com/example/mvi_compose_basic/activity/PagerActivity.kt
@@ -0,0 +1,112 @@
+package com.example.mvi_compose_basic.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.mvi_compose_basic.activity.ui.theme.MviComposeBasicTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
+
+@ExperimentalPagerApi
+class PagerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MviComposeBasicTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TabPagerView()
+                }
+            }
+        }
+    }
+}
+
+val pages = listOf("First", "Second", "Third")
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabPagerView() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        val pagerState = rememberPagerState()
+        val scope = rememberCoroutineScope()
+
+        TabRow(selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }) {
+            pages.forEachIndexed { index, title ->
+                Tab(
+                    text = { Text(text = title) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        scope.launch { pagerState.scrollToPage(index) }
+                    })
+            }
+        }
+
+        HorizontalPager(count = pages.size, state = pagerState) { page ->
+            Text(
+                text = page.toString(),
+                modifier = Modifier.wrapContentSize(),
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp
+            )
+        }
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun PagerView() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        HorizontalPager(count = 4, modifier = Modifier.fillMaxSize()) { page ->
+            Text(
+                text = page.toString(),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.wrapContentSize(),
+                fontSize = 30.sp,
+                fontStyle = FontStyle.Italic
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting6(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview6() {
+    MviComposeBasicTheme {
+        Greeting6("Android")
+    }
+}
--- ProgrammersTest/app/src/main/java/com/example/programmerstest/BaekJoon.kt
+++ ProgrammersTest/app/src/main/java/com/example/programmerstest/BaekJoon.kt
@@ -26,6 +26,21 @@ fun main3213() {
 }
 
 fun main() = with(Scanner(System.`in`)) {
+    val input = next()
+    fun getSubstrings(s: String): List<String> {
+        val substrings = mutableListOf<String>()
+        val n = s.length
+        for (i in 0 until n) {
+            for (j in i + 1..n) {
+                substrings.add(s.substring(i, j))
+            }
+        }
+        return substrings
+    }
+    println(getSubstrings(input).distinct().size)
+}
+
+fun mainGreedy() = with(Scanner(System.`in`)) {
     var input = nextInt()
     var cnt = 0
     while (true) {
