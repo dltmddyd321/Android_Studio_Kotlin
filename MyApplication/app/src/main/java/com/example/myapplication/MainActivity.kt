b5b3d7e3979633ae81204fe27a796ab7e7f9aa45
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-05-12 20:54:57 +0900

    Update MainActivity.kt

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -48,7 +48,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
+fun main() = with(System.out.bufferedWriter()){
+    val br = System.`in`.bufferedReader()
+    //홀SK 짝CY
+    var n = Integer.parseInt(br.readLine())
+    if(n and 1 ==1){
+        write("SK")
+    }
+    else{
+        write("CY")
+    }
+    close()
+}
 
 @Composable
 fun Greeting(name: String) {
