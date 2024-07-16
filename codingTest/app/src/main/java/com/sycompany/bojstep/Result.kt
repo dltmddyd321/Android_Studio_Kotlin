51e4f5a9d62bef055b945e696c3a0589d3ccc870
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-07-16 20:12:46 +0900

    Update Result.kt

--- codingTest/app/src/main/java/com/sycompany/bojstep/Result.kt
+++ codingTest/app/src/main/java/com/sycompany/bojstep/Result.kt
@@ -42,11 +42,39 @@ fun main() {
 }
 
 fun solution(today: String, terms: Array<String>, privacies: Array<String>): IntArray {
-    var answer: IntArray = intArrayOf()
-    return answer
+    val answer = mutableListOf<Int>()
+    val term = mutableMapOf<String, Int>()
+    val todayInt = today.replace(".", "").toInt()
+
+    terms.map {
+        val (type, date) = it.split(" ")
+        term[type] = date.toInt()
+    }
+
+    privacies.mapIndexed { i, it ->
+        val (date, type) = it.split(" ")
+        var year = term[type]!! / 12
+        if (year < 1)
+            year = 0
+
+        val month = term[type]!! % 12
+        val expiredDate = (date.replace(".", "").toInt() + year * 10000 + month * 100).toString()
+
+        var temp = expiredDate.toInt()
+        if (expiredDate[4].toString().toInt() * 10 + expiredDate[5].toString().toInt() > 12) {
+            temp -= 1200
+            temp += 10000
+        }
+
+        if (temp <= todayInt) {
+            answer.add(i + 1)
+        }
+    }
+
+    return answer.toIntArray()
 }
 
-fun stringToCalendar(dateString: String): Calendar {
+fun stringToCalendar(dateString: String, addMonth: Int? = null): Calendar {
     val sdf = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault())
     val calendar = Calendar.getInstance()
     try {
@@ -55,6 +83,7 @@ fun stringToCalendar(dateString: String): Calendar {
     } catch (e: Exception) {
         e.printStackTrace()
     }
+    if (addMonth != null) calendar.add(Calendar.MONTH, addMonth)
     return calendar
 }
 
