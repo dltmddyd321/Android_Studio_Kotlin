9fedf88c1c4d4a4a055972b3dc549bec6f7909f3
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-07-18 21:39:56 +0900

    코테 풀이

--- codingTest/app/src/main/java/com/sycompany/bojstep/DispatchGroup.kt
+++ codingTest/app/src/main/java/com/sycompany/bojstep/DispatchGroup.kt
@@ -31,3 +31,37 @@ class DispatchGroup {
         }
     }
 }
+
+fun main() {
+    val arr = arrayOf("..", "#.")
+    println(solution(arr).joinToString(""))
+}
+
+fun solution(wallpaper: Array<String>): IntArray {
+    data class Position(
+        val x: Int,
+        val y: Int
+    )
+
+    val list = mutableListOf<Position>()
+
+    wallpaper.forEachIndexed { indexX, xValue ->
+        xValue.forEachIndexed { indexY, yValue ->
+            if (yValue == '#') list.add(Position(indexX, indexY))
+        }
+    }
+    val minXPos = list.minOf { it.x }
+    val minYPos = list.minOf { it.y }
+    val startPos = Position(minXPos, minYPos)
+
+    val maxXPos = list.maxOf { it.x }
+    val maxYPos = list.maxOf { it.y }
+    val endPos = Position(maxXPos + 1, maxYPos + 1)
+
+    val res = mutableListOf<Int>()
+    res.add(startPos.x)
+    res.add(startPos.y)
+    res.add(endPos.x)
+    res.add(endPos.y)
+    return res.toIntArray()
+}
