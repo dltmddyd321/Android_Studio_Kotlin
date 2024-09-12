c2127521912c2ecdb84926455fcc8bd2eee3f98a
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-09-12 21:39:01 +0900

    2493번 탑 풀이
    - 스택

--- codingTest/app/src/main/java/com/sycompany/bojstep/Result.kt
+++ codingTest/app/src/main/java/com/sycompany/bojstep/Result.kt
@@ -31,24 +31,42 @@ import kotlin.math.min
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
-fun longestCommonSubstring(s1: String, s2: String): String {
-    val len1 = s1.length
-    val len2 = s2.length
-    var maxLength = 0
-    var endIndex = 0
+fun mainStack() {
+//    val stack = Stack<Int>()
+//    val n = readln().toInt()
+//    repeat(n) { stack.push(readln().toInt()) }
+//
+//    val map = mutableMapOf<Int, Int>()
+//
+//    while (stack.isNotEmpty()) {
+//        val item = stack.pop()
+//        if (!stack.any { it > item }) {
+//            map[item] = 0
+//        } else {
+//            if (item <= stack.peek()) stack.size
+//        }
+//    }
 
-    
+    val n = readln().toInt() // 탑의 수
+    val heights = readln().split(" ").map { it.toInt() } // 탑들의 높이
+    val result = IntArray(n) // 결과를 저장할 배열
+    val stack = Stack<Pair<Int, Int>>() // (탑의 인덱스, 탑의 높이)를 저장하는 스택
 
-    // 가장 긴 공통 부분 문자열 추출
-    return s1.substring(endIndex - maxLength, endIndex)
-}
+    for (i in heights.indices) {
+        while (stack.isNotEmpty() && stack.peek().second <= heights[i]) {
+            stack.pop() // 현재 탑보다 작은 탑들은 무시
+        }
 
-fun main() {
-    val s1 = readLine()!!
-    val s2 = readLine()!!
+        if (stack.isNotEmpty()) {
+            result[i] = stack.peek().first + 1 // 수신하는 탑의 인덱스를 저장 (1-based index)
+        } else {
+            result[i] = 0 // 수신하는 탑이 없으면 0을 저장
+        }
 
-    val result = longestCommonSubstring(s1, s2)
-    println(result)
+        stack.push(Pair(i, heights[i])) // 현재 탑을 스택에 추가
+    }
+
+    println(result.joinToString(" ")) // 결과 출력
 }
 
 @RequiresApi(Build.VERSION_CODES.N)
