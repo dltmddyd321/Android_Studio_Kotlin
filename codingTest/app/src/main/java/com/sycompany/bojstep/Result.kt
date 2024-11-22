e32994eb52ff473923b2811b18bd6a6e3bdbe1fc
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-11-22 21:13:54 +0900

    수 찾기 1920 이분 탐색 풀이

--- codingTest/app/src/main/java/com/sycompany/bojstep/Result.kt
+++ codingTest/app/src/main/java/com/sycompany/bojstep/Result.kt
@@ -52,9 +52,35 @@ data class Message(
     var count: Int
 )
 
+//이분 탐색을 통한 리스트 간 포함 여부 판별
 fun main() {
-    val (a, b, c) = readln().split(" ").map { it.toLong() }
-    println(powerMod(a, b, c))
+    // 입력
+    val n = readLine()!!.toInt()
+    val arrayA = readLine()!!.split(" ").map { it.toInt() }.sorted() // 정렬된 배열
+    val m = readLine()!!.toInt()
+    val arrayB = readLine()!!.split(" ").map { it.toInt() }
+
+    // 이분 탐색 함수
+    fun binarySearch(array: List<Int>, target: Int): Boolean {
+        var low = 0
+        var high = array.size - 1
+
+        while (low <= high) {
+            val mid = (low + high) / 2
+            when {
+                array[mid] == target -> return true // 찾았음
+                array[mid] < target -> low = mid + 1 // 오른쪽 탐색
+                else -> high = mid - 1 // 왼쪽 탐색
+            }
+        }
+        return false // 없음
+    }
+
+    // 배열 B의 각 원소에 대해 A에서 탐색
+    val result = arrayB.map { if (binarySearch(arrayA, it)) 1 else 0 }
+
+    // 출력
+    result.forEach { println(it) }
 }
 
 // 재귀 함수로 거듭 제곱 계산
